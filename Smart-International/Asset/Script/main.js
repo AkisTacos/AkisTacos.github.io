32dcd7e89ddbf18a51f672c532a752a3e03ded42
AkisTacos <ajsaito@ucdavis.edu>
2019-09-30 14:03:11 -0700

    Setup for bigwork

---
+++ Smart-International/Asset/Script/main.js
@@ -0,0 +1,10 @@
+// Developed by Aki Saito @ Smart International Services
+
+// Free Loading Icon Page Gif by smallenvelop.com
+document.onreadystatechange = function () {
+  var state = document.readyState
+  if (state == 'complete') {
+         document.getElementById('interactive');
+         document.getElementById('load').style.visibility="hidden";
+  }
+}
